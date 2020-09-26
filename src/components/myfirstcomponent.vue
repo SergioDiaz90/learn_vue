6d6cadfad6813f03f78b16746d1d126017d1a692
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-26 00:15:38 -0500

    [Add]: first part

---
+++ src/components/myfirstcomponent.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="content-wrapper">
+		<hr>
+		<h1>Hola desde el nuevo componente</h1>
+		<h2>{{ subtitulos }}</h2>
+		<hr/>
+	</div>
+</template>
+
+<script>
+export default {
+	name : 'myfirstcomponent',
+	data() {
+		return {
+			subtitulos : 'Soy el subtitulo del componente'
+		}
+	}
+}
+</script>
