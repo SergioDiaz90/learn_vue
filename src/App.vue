7a290cc44150e5c8b637c254524cfb46116d6b3e
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-26 01:38:38 -0500

    [Add]: New components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
     <header_component></header_component>
+    <Slider></Slider>
+    <div class="center">
+      <LastArticle></LastArticle>
+      <Sidebar></Sidebar>
+      <div class="clearfix"></div>
+    </div>
+    <FooterComponent></FooterComponent>
   </div>
 </template>
 
 <script>
-import header_component from './components/header_component'
+  import header_component from './components/header_component'
+  import Slider from './components/Slider'
+  import Sidebar from './components/Sidebar'
+  import LastArticle from './components/LastArticle'
+  import FooterComponent from './components/FooterContent'
 
-export default {
-  name: 'App',
-  components: {
-    header_component,
-  }
+  export default {
+    name: 'App',
+    components: {
+      header_component,
+      Slider,
+      Sidebar,
+      LastArticle,
+      FooterComponent,
+    }
 }
 </script>
 
